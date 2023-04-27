362aeeaf9a9b56f9e260d3b3f2f04d042073b38c
hugo-04 <edseldvegavega@gmail.com>
2023-04-26 22:57:38 -0500

    pedidos

--- src/main/java/controllers/PedidosController.java
+++ src/main/java/controllers/PedidosController.java
@@ -1,5 +1,42 @@
 package controllers;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.Pedidos;
+import services.PedidosServices;
+
+@RestController
+@RequestMapping("/pedidos")
 public class PedidosController {
-
+	  @Autowired
+	    private PedidosServices pedidosServicios;
+	    
+	    @GetMapping("/")
+	    public List<Pedidos> obtenerPedidos() {
+	        return pedidosServicios.obtenerPedidos();
+	    }
+	    
+	    @PostMapping("/")
+	    public void agregarPedido(@RequestBody Pedidos pedidos) {
+	        pedidosServicios.agregarPedidos(pedidos);
+	    }
+	    
+	    @PutMapping("/")
+	    public void actualizarPedido(@RequestBody Pedidos pedidos) {
+	        pedidosServicios.actualizarPedidos(pedidos);
+	    }
+	    
+	    @DeleteMapping("/{idPedido}")
+	    public void eliminarPedido(@PathVariable Long idPedidos) {
+	        pedidosServicios.eliminarPedido(idPedidos);
+	    }
 }
--- src/main/java/models/Pedidos.java
+++ src/main/java/models/Pedidos.java
@@ -1,5 +1,79 @@
 package models;
 
-public interface Pedidos {
+import java.io.Serializable;
+import java.sql.Date;
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+;
+
+@Entity
+@Table(name ="productos")
+
+public class Pedidos implements Serializable{
+	private static final long serialVersionUID = 1L; 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Basic(optional = false)
+    @Column(name = "idPedidos")
+    private Integer idPedidos;
+
+    @Column(name = "fecha")
+    private Date fecha;
+
+    @Column(name = "idCliente")
+    private int idCliente;
+
+    @Column(name = "idProducto")
+    private int idProducto;
+
+    public Pedidos() {}
+
+    public Pedidos(Integer idpedidos, Date fecha, int idcliente, int idproducto) {
+    
+        this.idPedidos =  idpedidos;
+        this.fecha =  fecha;
+        this.idCliente =  idcliente;
+        this.idProducto =  idproducto;
+    }
+
+    public Integer getidPedidos() {
+        return idPedidos;
+    }
+
+    public void setidPedidos(Integer idPedidos) {
+        this.idPedidos = idPedidos;
+    }
+
+    public Date getfecha() {
+        return fecha;
+    }
+
+    public void setfecha(Date fecha) {
+        this.fecha = fecha;
+    
+    }
+    public int getidCliente() {
+        return idCliente;
+    }
+
+    public void setidCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public int getidProducto() {
+        return idProducto;
+    }
+
+    public void setidProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
 
 }
